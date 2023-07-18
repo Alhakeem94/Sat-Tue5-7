2015d6f8e258478ab0aa663c9d4425da3fa7b9c9
mohammed alhakeem <alhakeem94mohammed@gmail.com>
2023-07-18 18:30:05 +0300

    AddedJavaScript

---
+++ Sat-Tue5-7/wwwroot/JsScripts/FilesScripts.js
@@ -0,0 +1,26 @@
+﻿function OpenNewTab(pdfFilePath) {
+    window.open(pdfFilePath)
+}
+
+window.saveAsFile = function (fileName, byteBase64) {
+    var link = this.document.createElement('a');
+    link.download = fileName;
+    link.href = "data:application/octet-stream;base64," + byteBase64;
+    this.document.body.appendChild(link);
+    link.click();
+    this.document.body.removeChild(link);
+}
+
+window.downloadFileFromStream = async (fileName, contentStreamReference) => {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+}
+
+
